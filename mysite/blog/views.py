27a81a76590cecd17b2e50c223597437c4bcfb81
CYCSICS <sz597002139@outllook.com>
2018-11-19 23:01:33 +0800

    Add register

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -50,12 +50,13 @@ def blogAdd(request):
         print(blogInfo)
         blogCreate = models.BlogsPost(
             title = blogInfo['Title'],
+            simple = blogInfo['content'][:50],
             body = blogInfo['content'],
             timestamp = blogInfo['Time']
         )
         blogCreate.save()
         msg = "成功上传博客"
-        return render(request,'success.html',{'msg':msg})
+        return render(request,'report.html',{'msg':msg,'urlname':'blogAdd'})
 
 def blogIndex(request):
     if request.method == 'GET':
@@ -64,10 +65,15 @@ def blogIndex(request):
     if request.method == 'POST':
         userdata = {}
         userdata['username'] = request.POST['username']
-        userdata['pws'] = request.POST['password']
-        msg = "用户{0}成功登陆".format(userdata['username'])
-        user = User.objects.filter(username__exact = username,password__exact = password)
-        return render(request, "success.html", msg)
+        userdata['psw'] = request.POST['password']
+        
+        user = User.objects.filter(username = userdata['username'],password = userdata['psw'])
+        if user:
+            msg = "用户{0}成功登陆".format(userdata['username'])
+            return render(request, "report.html", {'msg':msg})
+        else:
+            msg = "登陆失败，请重新登陆"
+            return render(request,"report.html",{'msg':msg})
         # uf = UserForm(request.POST)
         # if uf.is_valid():
         #     #获取表单用户密码
@@ -90,7 +96,7 @@ def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('Password')
-        user = User.objects.filter(username__exact = username,password__exact = password)
+        user = User.objects.filter(username = username,password = password)
         if user:
             return render_to_response('index.html',{'username':username,'blog_list':blog_list})
     else:
@@ -119,3 +125,22 @@ def addBlog(request):
         print(title)
         print(content)
         return render(request, 'index.html', locals())
+
+def register(request):
+    if request.method == 'GET':
+        return render(request,'register.html',{})
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 != password2:
+            msg = "请输入一样的密码"
+            return render(request,'register.html',{'msg':msg})
+        else:
+            userPost = models.User(
+                username = username,
+                password = password1
+            )
+            userPost.save()
+            msg = "成功注册用户,用户名为：{0}".format(username)
+            return render(request,'report.html',{'msg':msg,'urlname':'register'})
